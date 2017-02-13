3603a100fefeecfdc3298f35efb7fac6e887369b
luisplazas <lf.plazas10@uniandes.edu.co>
2017-02-12 23:22:28 -0500

    Trash deletion

--- app/Global.java
+++ app/Global.java
@@ -29,54 +29,7 @@ import java.util.concurrent.TimeUnit;
 public class Global extends GlobalSettings {
     @Override
     public void onStart(Application app) {
-//        Akka.system().scheduler().scheduleOnce(
-//                Duration.create(0, TimeUnit.MILLISECONDS),
-//                new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        Logger.info("ON START ---    " + System.currentTimeMillis());
-//                    }
-//                }
-//        );
-//
-//        FiniteDuration duration = Duration.create((long) 10, TimeUnit.SECONDS);
-//
-//        Akka.system().scheduler().scheduleOnce(
-//                duration,
-//                () -> {
-//                    System.out.println();;
-//                }, Akka.system().dispatcher()
-//        );
-//
-//        Akka.system().scheduler().schedule(
-//                Duration.create(nextExecutionInSeconds(8, 0), TimeUnit.SECONDS),
-//                Duration.create(24, TimeUnit.HOURS),
-//                new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        Logger.info("EVERY DAY AT 8:00 ---    " + System.currentTimeMillis());
-//                    }
-//                }
-//        );
-    }
 
-    public static int nextExecutionInSeconds(int hour, int minute){
-        return Seconds.secondsBetween(
-                new DateTime(),
-                nextExecution(hour, minute)
-        ).getSeconds();
-    }
-
-    public static DateTime nextExecution(int hour, int minute){
-        DateTime next = new DateTime()
-                .withHourOfDay(hour)
-                .withMinuteOfHour(minute)
-                .withSecondOfMinute(0)
-                .withMillisOfSecond(0);
-
-        return (next.isBeforeNow())
-                ? next.plusHours(24)
-                : next;
     }
 
 }
